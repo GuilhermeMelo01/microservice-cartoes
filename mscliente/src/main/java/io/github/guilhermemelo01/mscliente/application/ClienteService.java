d7ae7aec1dd13cee6a59ed22f42874974a268505
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-07-13 14:47:03 -0300

    Implementando o microservice de clientes

---
+++ mscliente/src/main/java/io/github/guilhermemelo01/mscliente/application/ClienteService.java
@@ -0,0 +1,25 @@
+package io.github.guilhermemelo01.mscliente.application;
+
+import io.github.guilhermemelo01.mscliente.domain.Cliente;
+import io.github.guilhermemelo01.mscliente.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente save(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCpf(String cpf){
+        return clienteRepository.findByCpf(cpf);
+    }
+}
---
+++ mscliente/src/main/java/io/github/guilhermemelo01/mscliente/domain/Cliente.java
@@ -0,0 +1,45 @@
+package io.github.guilhermemelo01.mscliente.domain;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente(String cpf, String nome, Integer idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Cliente cliente = (Cliente) o;
+        return id != null && Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
