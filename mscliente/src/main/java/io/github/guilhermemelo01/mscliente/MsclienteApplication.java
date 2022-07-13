68df5cc1ff42a0ea3a33b015115aed72033b51e6
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-07-13 14:01:25 -0300

    Scaffolding do Microservice de Clientes

---
+++ mscliente/src/main/java/io/github/guilhermemelo01/mscliente/MsclienteApplication.java
@@ -0,0 +1,13 @@
+package io.github.guilhermemelo01.mscliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclienteApplication.class, args);
+	}
+
+}
