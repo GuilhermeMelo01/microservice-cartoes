2e68bb155a719b5d38cd2f95ad9f5301e12f3e17
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-07-20 14:24:27 -0300

    Implementando o microservice de Cartões parte 2

--- mscartoes/src/main/java/io/github/guilhermemelo01/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/io/github/guilhermemelo01/mscartoes/application/CartoesResource.java
@@ -5,10 +5,9 @@ import io.github.guilhermemelo01.mscartoes.domain.Cartao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("cartoes")
@@ -29,5 +28,10 @@ public class CartoesResource {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @RequestMapping(params = "renda", method = RequestMethod.GET)
+    public ResponseEntity<List<Cartao>> getCartoesRendaAteh(@RequestParam("renda") Long renda){
+        List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
+    }
 
 }
