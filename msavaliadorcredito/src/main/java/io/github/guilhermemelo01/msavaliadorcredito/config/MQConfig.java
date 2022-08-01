943a0584696df9e7340d8d73ff12fff510785021
GuilhermeMelo01 <meloguilherme1994@gmail.com>
2022-08-01 14:22:45 -0300

    Configurando o Resource server no Gateway

--- msavaliadorcredito/src/main/java/io/github/guilhermemelo01/msavaliadorcredito/config/MQConfig.java
+++ msavaliadorcredito/src/main/java/io/github/guilhermemelo01/msavaliadorcredito/config/MQConfig.java
@@ -2,6 +2,7 @@ package io.github.guilhermemelo01.msavaliadorcredito.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,6 +12,7 @@ public class MQConfig {
     private String emissaoCartoesFila;
 
     //Aqui vai ser a queue onde eu vou mandar publicar minhas mensagens
+    @Bean
     public Queue queueEmissaoCartoes(){
         return new Queue(emissaoCartoesFila, true);
     }
