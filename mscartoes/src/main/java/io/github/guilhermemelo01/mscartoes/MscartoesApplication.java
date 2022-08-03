02daca4cd9ce720ce64bdf12269856d217cb6a2d
GuilhermeMelo01 <meloguilherme1994@gmail.com>
2022-08-03 09:24:40 -0300

    logs com Slf4j

--- mscartoes/src/main/java/io/github/guilhermemelo01/mscartoes/MscartoesApplication.java
+++ mscartoes/src/main/java/io/github/guilhermemelo01/mscartoes/MscartoesApplication.java
@@ -1,5 +1,6 @@
 package io.github.guilhermemelo01.mscartoes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +9,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient //Mostra que esse MS é um cliente eureka
 @EnableRabbit //O Boot faz a configuracao do RabbitMQ
+@Slf4j
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
+		log.info("Iformação: {}", "teste info");
+		log.error("Error: {}", "teste error");
+		log.warn("Aviso: {}", "teste warn");
 		SpringApplication.run(MscartoesApplication.class, args);
 	}
 
